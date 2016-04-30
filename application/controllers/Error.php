3ea089159b0e1f02e3f045f52473e0818b62fd87
lxy <lxy@entstudy.com>
2016-04-30 11:57:36 +0800

    error controller

---
+++ application/controllers/Error.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lixiangyang
+ * Date: 16/4/30
+ * Time: 11:46
+ */
+
+class ErrorController extends Yaf\Controller_Abstract {
+
+    public function errorAction($exception) {
+        die($exception->getMessage());
+    }
+}
